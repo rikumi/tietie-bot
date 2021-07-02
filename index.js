79c08238e3276aeed7cf51279563f2af070f98e3
rikumiyu <rikumiyu@tencent.com>
2021-07-02 15:36:38 +0800

    feat: 分成 router 模式

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Telegraf } = require('telegraf');
 const config = require('./config.json');
+const fs = require('fs');
 
 const bot = new Telegraf(config.telegramBotToken);
 
@@ -10,20 +11,12 @@ const msgOptions = {
 
 bot.on('message', (ctx) => {
   const { message } = ctx;
-  if (!message.text || !message.text.startsWith('/') || /^\/[\w\-]+ /.test(message.text)) return;
-  const escape = (text) => text.replace(/([\u0000-\u00ff])/g, '\\$1');
-  const formatUser = (user, customName) => `[${escape(customName || user.first_name)}](https://t.me/${user.username})`;
-  const extractUser = (message, entity) => ({ first_name: message.text.substr(entity.offset, entity.length), username: entity.url.split('/').pop() });
-  const sender = message.from;
-  const senderLink = formatUser(sender);
-  const replied = message.reply_to_message;
-  const repliedBotMsg = replied && replied.from.username === bot.botInfo.username ? replied : undefined;
-  const lastMentionEntity = repliedBotMsg && (repliedBotMsg.entities || []).filter(k => k.type === 'text_link')[0];
-  const lastMentionUser = lastMentionEntity && extractUser(repliedBotMsg, lastMentionEntity);
-  const receiver = lastMentionUser || replied && replied.from || sender;
-  const receiverLink = formatUser(receiver, receiver.username === sender.username ? '自己' : undefined);
-  const action = message.text.slice(1);
-  ctx.reply(`${senderLink} ${action}了 ${receiverLink}！`, msgOptions);
+  if (!message.text || !message.text.startsWith('/')) return;
+  const action = message.text.split(' ')[0].slice(1);
+  let module = `./commands/${action}.js`;
+  if (!fs.existsSync(module)) module = `./commands/default.js`;
+  const result = require(module)(ctx, bot);
+  ctx.reply(result, msgOptions);
 });
 
 bot.launch();
