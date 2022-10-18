d2f1c2046f74238da167debb583844f5dfa4f6f2
Alendia <alendia0807@gmail.com>
2022-10-18 20:46:13 +0800

    feat: 后宫选择器

---
+++ commands/pick.js
@@ -0,0 +1,12 @@
+const msgOptions = {
+  parse_mode: "MarkdownV2",
+  disable_web_page_preview: true,
+};
+
+module.exports = (ctx, bot) => {
+  const content = ctx.message.text.split(/\s+/).slice(1);
+  const escape = (text) => text.replace(/([\u0000-\u00ff])/g, "\\$1");
+  if (content.length === 0) return;
+  const random = Math.floor(content.length * Math.random());
+  ctx.reply(content.map((e) => escape(e))[random], { ...msgOptions, reply_to_message_id: ctx.message.message_id });
+};
