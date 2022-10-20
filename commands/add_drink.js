b3ac453ee809f1186328931a27b3c1170cb4d46a
Alendia <alendia0807@gmail.com>
2022-10-20 20:39:33 +0800

    feat: 猫群怎么可以看米群喜欢的饮料呢

--- commands/add_drink.js
+++ commands/add_drink.js
@@ -10,13 +10,14 @@ module.exports = async (ctx, bot) => {
         .map((e) => escape(e))
     )
   );
+  const groupId = ctx.message.chat.id;
   if (content.length === 0) return '不可以什么都不加👊';
   if (content.length > 10) return '什么几把玩意儿，一次加这么多？';
   if (content.some((e) => e.length > 10)) return '什么几把玩意儿，一次加这么长？';
-  return checkDrinks(content).then(async (value) => {
+  return checkDrinks(content, groupId).then(async (value) => {
     console.log(value);
     if (value.some((e) => e !== undefined)) return '这个已经有了👊';
-    const result = await addDrink(content);
+    const result = await addDrink(content, groupId);
     return result ? `添加了 ${content.join('，')}！` : `添加失败了😭`;
   }, console.log);
 };
--- db.js
+++ db.js
@@ -5,15 +5,16 @@ const startDatabase = () => {
   db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS drinks (
       id INTEGER PRIMARY KEY,
-      name TEXT NOT NULL
+      name TEXT NOT NULL,
+      group_id INTEGER NOT NULL
     )`);
   });
 };
 
-const checkDrinks = (names) => {
+const checkDrinks = (names, groupId) => {
   const length = names.length;
   return new Promise((res, rej) => {
-    db.all(`SELECT name FROM drinks WHERE name IN (${Array(length).fill('?').join(', ')})`, names, (err, row) => {
+    db.all(`SELECT name FROM drinks WHERE name IN (${Array(length).fill('?').join(', ')}) AND group_id = ?`, [...names, groupId], (err, row) => {
       if (err) {
         rej(err);
       } else {
@@ -24,10 +25,10 @@ const checkDrinks = (names) => {
   });
 };
 
-const addDrink = (names) => {
+const addDrink = (names, groupId) => {
   return new Promise((res, rej) => {
     names.forEach((name) => {
-      db.run(`INSERT INTO drinks (name) VALUES (?)`, name, (err) => {
+      db.run(`INSERT INTO drinks (name, group_id) VALUES (?, ?)`, [name, groupId], (err) => {
         if (err) {
           rej(err);
         } else {
@@ -38,9 +39,9 @@ const addDrink = (names) => {
   });
 };
 
-const showDrinks = () => {
+const showDrinks = (groupId) => {
   return new Promise((res, rej) => {
-    db.all(`SELECT name FROM drinks`, (err, rows) => {
+    db.all(`SELECT name FROM drinks WHERE group_id = ?`, groupId, (err, rows) => {
       const drinkNames = rows.map((e) => e.name);
       res(drinkNames);
     });
