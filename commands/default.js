0607ec054013e872d4a85f90acc115b2d6934671
Rikumi <riku.mi@outlook.com>
2022-12-05 11:55:16 +0800

    Update default.js

--- commands/default.js
+++ commands/default.js
@@ -2,12 +2,12 @@ module.exports = (ctx, bot) => {
   const { message } = ctx;
   if (!/[^\u0000-\u00ff]/.test(message.text.split(/\s+/)[0])) return;
   const escape = (text) => text.replace(/([\u0000-\u00ff])/g, '\\$1');
-  const getUrl = (username) => (username ? `https://t.me/${username}` : 'https://youtu.be/dQw4w9WgXcQ');
+  const getUrl = ({ id, username }) => (username ? `https://t.me/${username}` : `tg://user?id=${id}`);
   const formatUser = (user, customName) =>
-    `[${escape(customName || `${user.first_name} ${user.last_name || ''}`.trim())}](${getUrl(user.username)})`;
+    `[${escape(customName || `${user.first_name} ${user.last_name || ''}`.trim())}](${getUrl(user)})`;
   const extractUser = (message, entity) => ({
     first_name: message.text.substr(entity.offset, entity.length),
-    username: entity.url.split('/').pop(),
+    username: entity.url.split(/[/=]/).pop(),
   });
   const sender = message.from;
   const senderLink = formatUser(sender);
