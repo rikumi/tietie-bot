ebfcfc1aba49aded14ff261b21e0d7d536a203e4
Rikumi <riku.mi@outlook.com>
2022-12-05 13:40:40 +0800

    Create chatgpt.js

---
+++ commands/chatgpt.js
@@ -0,0 +1,15 @@
+const chatgpt = require('chatgpt-lib');
+const { chatgptSessionToken } = require('../config.json');
+
+const msgOptions = {
+  parse_mode: 'MarkdownV2',
+  disable_web_page_preview: true,
+};
+
+const escape = (text) => text.replace(/([\u0000-\u00ff])/g, '\\$1');
+const chatbot = new cgpt.ChatGPT({ SessionToken: chatgptSessionToken });
+
+module.exports = (ctx, bot) => {
+  const { message } = ctx;
+  chatbot.ask(message).then(answer => ctx.reply(answer, msgOptions));
+};
