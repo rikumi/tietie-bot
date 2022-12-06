01e3d099ae6265b992a09b0362471f35477167c9
Rikumi <riku.mi@outlook.com>
2022-12-06 17:17:51 +0800

    Update chatgpt.js

--- commands/chatgpt.js
+++ commands/chatgpt.js
@@ -12,8 +12,7 @@ module.exports = (ctx, bot) => {
   ctx.reply('ChatGPT 正在思考…', { reply_to_message_id: message.message_id }).then(async replyMessage => {
     try {
       const answer = await chatbot.ask(message.text.replace(/^\/chatgpt\s+/, ''));
-      const trimmedAnswer = answer.length > 512 ? answer.slice(0, 512) + '...' : answer;
-      ctx.telegram.editMessageText(chatId, replyMessage.message_id, undefined, trimmedAnswer);
+      ctx.telegram.editMessageText(chatId, replyMessage.message_id, undefined, answer);
     } catch (e) {
       console.error(e);
       ctx.telegram.editMessageText(chatId, replyMessage.message_id, undefined, '请求失败了，可能是接口被限频或者 token 失效，请过一会再问我这个问题。');
