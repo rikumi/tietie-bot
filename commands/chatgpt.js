d9bb41ec7b77db92c0ea98ca92a7382499c071f7
rikumiyu <rikumiyu@tencent.com>
2022-12-07 23:58:01 +0800

    feat: 问题不能为空

--- commands/chatgpt.js
+++ commands/chatgpt.js
@@ -9,11 +9,16 @@ module.exports = async (ctx) => {
     ctx.reply('当前会话未设置 Token，请使用 /chatgpt_token <session_token> 设置。Token 需要在 chat.openai.com/chat 页面上获取 Cookie 获得。', { reply_to_message_id: message.message_id });
     return;
   }
+  const question = message.text.trim().split(/\s+/)[1];
+  if (!question) {
+    ctx.reply('问题不能为空。', { reply_to_message_id: message.message_id });
+    return;
+  }
   const chatbot = ChatGPT.getInstance(token);
   const replyMessage = await ctx.reply('ChatGPT 正在思考…', { reply_to_message_id: message.message_id });
   try {
     let lastAnswer = '';
-    for await (const answer of chatbot.ask(message.text.trim().split(/\s+/)[1])) {
+    for await (const answer of chatbot.ask(question)) {
       await Promise.all([
         ctx.telegram.editMessageText(chatId, replyMessage.message_id, undefined, answer + '…'),
         new Promise(r => setTimeout(r, 1000)),
