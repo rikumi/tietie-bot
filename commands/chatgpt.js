07828e3af59cb3ad4261f0af8640434981e89a6f
rikumiyu <rikumiyu@tencent.com>
2022-12-09 19:43:13 +0800

    feat: too many requests

--- commands/chatgpt.js
+++ commands/chatgpt.js
@@ -14,20 +14,31 @@ module.exports = async (ctx) => {
   }
   const chatbot = ChatGPT.getInstance(token);
   const replyMessage = await ctx.reply('…', { reply_to_message_id: message.message_id });
-  const setTyping = () => ctx.telegram.sendChatAction(chatId, 'typing');
-  await setTyping();
+
+  const editMessage = async (text) => {
+    try {
+      ctx.telegram.editMessageText(chatId, replyMessage.message_id, undefined, text);
+    } catch (e) {
+      if (e.message && e.message.includes('Too Many Requests')) {
+        await new Promise(r => setTimeout(r, 1000));
+        return editMessage(text);
+      };
+    }
+  };
+
   try {
     let lastAnswer = '';
     for await (const answer of chatbot.ask(question)) {
+      if (!answer) continue;
       await Promise.all([
-        ctx.telegram.editMessageText(chatId, replyMessage.message_id, undefined, answer + '…').then(setTyping),
-        new Promise(r => setTimeout(r, 5000)),
+        editMessage(answer + '…'),
+        new Promise(r => setTimeout(r, 1000)),
       ]);
       lastAnswer = answer;
     }
-    await ctx.telegram.editMessageText(chatId, replyMessage.message_id, undefined, lastAnswer);
+    await editMessage(lastAnswer);
   } catch (e) {
     console.error(e);
-    ctx.telegram.editMessageText(chatId, replyMessage.message_id, undefined, '请求失败了，可能是接口被限频或者 token 失效，请过一会再问我这个问题。\n' + e.message);
+    editMessage('请求失败了，可能是接口被限频或者 token 失效，请过一会再问我这个问题。\n' + e.message);
   }
 };
