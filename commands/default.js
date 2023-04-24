d028a279f93a690aa8043b79ba85213488b9c9b9
rikumiyu <rikumiyu@tencent.com>
2023-04-24 16:47:55 +0800

    feat: impart mode

--- commands/default.js
+++ commands/default.js
@@ -1,3 +1,5 @@
+const impart = require('./impart');
+
 const msgOptions = {
   parse_mode: 'MarkdownV2',
   disable_web_page_preview: true,
@@ -12,15 +14,45 @@ module.exports = (ctx, bot) => {
     first_name: message.text.substr(entity.offset, entity.length),
     id: entity.url.split(/=/).pop(),
   });
-  const sender = message.from;
-  const senderLink = formatUser(sender);
+
   const replied = message.reply_to_message;
   const repliedBotMsg = replied && replied.from.username === bot.botInfo.username ? replied : undefined;
   const lastMentionEntity = repliedBotMsg && (repliedBotMsg.entities || []).filter((k) => k.type === 'text_link')[0];
   const lastMentionUser = lastMentionEntity && extractUser(repliedBotMsg, lastMentionEntity);
-  const receiver = lastMentionUser || (replied && replied.from) || sender;
+
+  let sender = message.from;
+  let receiver = lastMentionUser || (replied && replied.from) || sender;
+  let impartImpact = false;
+
+  if (impart.isInImpart(message.chat.id) && sender.id !== receiver.id) {
+    const random = Math.random();
+    const originalReceiver = receiver;
+    // 50% 概率变为自己操作自己
+    if (random > 0.5) {
+      receiver = sender;
+      impartImpact = true;
+    }
+    // 25% 概率变为对方操作自己
+    if (random > 0.75) {
+      sender = originalReceiver;
+      impartImpact = true;
+    }
+    // 2% 概率变为对方操作对方
+    if (random < 0.02) {
+      sender = originalReceiver;
+      impartImpact = true;
+    }
+  }
+
+  const senderLink = formatUser(sender);
   const receiverLink = formatUser(receiver, receiver.id === sender.id ? '自己' : undefined);
   const [action, ...rest] = message.text.slice(1).split(/\s+/).map(escape);
   const postfix = rest.join(' ').trim();
-  ctx.reply(`${senderLink} ${action}${postfix ? '' : '了'} ${receiverLink} ${postfix}`.trim() + '！', msgOptions);
+  let result = `${senderLink} ${action}${postfix ? '' : '了'} ${receiverLink} ${postfix}`.trim() + '！';
+
+  if (impartImpact) {
+    result += '\n\n\\#ImpartImpact';
+  }
+
+  ctx.reply(result, msgOptions);
 };
