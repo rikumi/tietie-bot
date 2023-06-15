e6c1e7af0de1cdb3cbc9cd264980bd695b5f5404
Rikumi <riku.mi@outlook.com>
2023-06-15 14:09:48 +0800

    Update me.js

--- commands/me.js
+++ commands/me.js
@@ -1,7 +1,13 @@
+const msgOptions = {
+  parse_mode: 'MarkdownV2',
+  disable_web_page_preview: true,
+};
+
 module.exports = async (ctx) => {
   const { message } = ctx;
   const content = message.text.trim().replace(/^.*?\s+/, '');
   if (!content) return;
+  const escape = (text) => text.replace(/([\u0000-\u00ff])/g, '\\$1');
   const formatUser = (user, customName) => `[${escape(customName || `${user.first_name} ${user.last_name || ''}`.trim())}](tg://user?id=${user.id})`;
-  return `${formatUser(message.from)} ${content}！`
+  ctx.reply(`${formatUser(message.from)} ${escape(content)}！`, msgOptions);
 };
