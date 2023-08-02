1fba9ee265d7a3b0f3c6f8642e81214a6911789f
rikumiyu <rikumiyu@tencent.com>
2023-08-02 19:13:21 +0800

    feat: update

---
+++ commands/update.js
@@ -0,0 +1,15 @@
+const cp = require('child_process');
+
+const exec = async (command, args) => {
+  await new Promise((resolve, reject) => {
+    const child = cp.spawn(command, args);
+    child.on('exit', (code) => code ? reject(`Process ${command} exited with code ${code}`) : resolve());
+  });
+};
+
+module.exports = async (ctx) => {
+  await ctx.reply('代码更新执行中');
+  await exec('git', ['pull']);
+  await ctx.reply('代码更新执行完成，即将重启');
+  cp.spawn('npm', ['run', 'restart']).unref();
+};
