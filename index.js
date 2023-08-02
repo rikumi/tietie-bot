a040205b1ac4d13fba7a550e1f2ed6a8ed21795b
rikumiyu <rikumiyu@tencent.com>
2023-08-02 19:02:24 +0800

    fix: 阻塞

--- index.js
+++ index.js
@@ -23,12 +23,7 @@ const handleMessage = async (ctx) => {
   }
 };
 
-bot.on('message', (ctx) => {
-  if (ctx.message.date * 1000 < Date.now() - 10000) return;
-  handleMessage(ctx);
-});
-
-bot.on('callback_query', async (ctx) => {
+const handleCallbackQuery = async (ctx) => {
   const { message } = ctx;
   const moduleName = ctx.callbackQuery.data.split(':')[0];
   const module = `./commands/${moduleName}.js`;
@@ -39,6 +34,15 @@ bot.on('callback_query', async (ctx) => {
     console.error(e);
     ctx.reply('Error: ' + e.message, { reply_to_message_id: message.message_id });
   }
+};
+
+bot.on('message', (ctx) => {
+  if (ctx.message.date * 1000 < Date.now() - 10000) return;
+  handleMessage(ctx);
+});
+
+bot.on('callback_query', (ctx) => {
+  handleCallbackQuery(ctx);
 });
 
 bot.launch().then(() => {
