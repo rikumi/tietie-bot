3fa70c530abc2d7f08fa6e14293db94d51f4b05e
rikumiyu <rikumiyu@tencent.com>
2023-08-02 19:17:46 +0800

    fix: update

--- commands/update.js
+++ commands/update.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const cp = require('child_process');
 
 const exec = async (command, args) => {
@@ -11,5 +12,7 @@ module.exports = async (ctx) => {
   await ctx.reply('代码更新执行中');
   await exec('git', ['pull']);
   await ctx.reply('代码更新执行完成，即将重启');
-  cp.spawn('npm', ['run', 'restart']).unref();
+  cp.spawn('npm', ['run', 'restart'], {
+    cwd: path.resolve(__dirname, '..'),
+  }).unref();
 };
