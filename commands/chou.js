48f9fbec9e9c05ae34268e9ed12eb06836b5ace3
Rikumi <riku.mi@outlook.com>
2023-08-07 01:34:04 +0800

    Create chou.js

---
+++ commands/chou.js
@@ -0,0 +1,20 @@
+module.exports = () => {
+  return Array(10).fill().map(() => {
+    const rand = Math.random();
+    if (rand < 0.01) {
+      return 5;
+    }
+    if (rand < 0.1) {
+      return 4;
+    }
+    if (rand < 0.3) {
+      return 3;
+    }
+    if (rand < 0.5) {
+      return 2;
+    }
+    return 1;
+  }).sort((a, b) => b - a).map(k => {
+    return ' ⬛🟫🟦🟪🟨'[k];
+  });
+};
