d71512fbf52c7f63af5bb7cb0325dc4aca182a63
Rikumi <riku.mi@outlook.com>
2023-08-14 23:55:16 +0800

    Update alias.js

--- commands/alias.js
+++ commands/alias.js
@@ -1,18 +1,19 @@
 const { setAlias, getAlias } = require('../modules/database');
 
 module.exports = async (ctx) => {
+  const groupId = ctx.message.chat.id;
   const [name, target] = ctx.message.text.split(/\s+/).slice(1);
   
   if (!name || !/^([a-z]+_)*[a-z]+$/.test(name)) {
     return '别名无效。';
   }
   if (!target) {
-    await setAlias(name, '');
+    await setAlias(groupId, name, '');
     return '已清除该别名。';
   }
   if (!/^([a-z]+_)*[a-z]+$/.test(target)) {
     return '别名指向的指令无效。';
   }
-  await setAlias(name, target);
+  await setAlias(groupId, name, target);
   return '已成功设置别名。';
 };
