5fa8ca26df450cd25ed3b7994ca4c6155bc9a7ee
rikumiyu <rikumiyu@tencent.com>
2023-09-10 00:15:49 +0800

    punycode

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Telegraf } = require('telegraf');
 const config = require('./config.json');
 const fs = require('fs');
+const punycode = require('punycode');
 const { startDatabase, getAlias, appendCharacter } = require('./modules/database');
 
 process.on('uncaughtException', (e) => { console.error(e); });
@@ -13,15 +14,17 @@ const handleMessage = async (ctx) => {
   const { message } = ctx;
 
   // 私聊转发聊天记录：添加到人物设定集
-  if (message.chat && message.chat.type === 'private' && message.forward_from) {
-    const username = message.forward_from.username || 'user_' + message.forward_from.id;
+  if (message.chat && message.chat.type === 'private' && (message.forward_from || message.forward_sender_name)) {
+    const username = message.forward_from
+      ? message.forward_from.username || 'user_' + message.forward_from.id
+      : punycode.encode(message.forward_sender_name.toLowerCase()).replace(/[\-\s]/g, '_');
     await appendCharacter(username, message.text, message.from.id);
     if (batchForwardReplyTimeoutMap[message.from.id]) {
       clearTimeout(batchForwardReplyTimeoutMap[message.from.id]);
       delete batchForwardReplyTimeoutMap[message.from.id];
     }
     batchForwardReplyTimeoutMap[message.from.id] = setTimeout(() => {
-      ctx.reply(`已将以上转发内容添加到各自发送者的人设集，可输入 /${username} 等进行尝试`);
+      ctx.reply(`已将以上转发内容添加到各自发送者的人设集，可输入 /${username} 进行尝试；转发单条消息可查询用户对应的指令名`);
     }, 1000);
     return;
   }
