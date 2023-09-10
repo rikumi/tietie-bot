207e7057a4b4f275e09e342cc1033d634639fb6f
Rikumi <riku.mi@outlook.com>
2023-09-10 04:26:57 -0500

    Update character.js

--- commands/character.js
+++ commands/character.js
@@ -37,8 +37,12 @@ const generateUsernames = (user) => {
   if (user.id) {
     usernames.push('user_' + user.id);
   }
-  usernames.push(toPinyin([user.first_name, user.last_name].filter(k => k).join(' ')));
-  usernames.push(toPinyin([user.last_name, user.first_name].filter(k => k).join(' ')));
+  if (user.first_name && user.last_name) {
+    usernames.push(toPinyin(user.first_name + ' ' + user.last_name));
+    usernames.push(toPinyin(user.last_name + ' ' + user.first_name));
+  } else {
+    usernames.push(toPinyin(user.first_name || user.last_name));
+  }
   return usernames;
 };
 
