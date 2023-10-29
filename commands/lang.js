ef688105c90385cd1d1b7bdfffb14383d45b15e2
Rikumi <riku.mi@outlook.com>
2023-10-29 10:08:02 +0800

    Update lang.js

--- commands/lang.js
+++ commands/lang.js
@@ -1,9 +1,9 @@
 module.exports = async (ctx) => {
   const { message } = ctx;
-  if (!message || !message.replied_to_message) {
+  if (!message || !message.reply_to_message) {
     return '请引用一条消息进行操作。';
   }
-  return message.replied_to_message.entities
+  return message.reply_to_message.entities
     .map(k => k.language)
     .filter(k => k)
     .join(', ') || '没有找到代码块。';
