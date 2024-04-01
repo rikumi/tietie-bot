854725a20d932f98baadfd9ecca088c262e1f774
rikumiyu <rikumiyu@tencent.com>
2024-04-01 12:26:50 +0800

    finally

--- src/commands/discord.ts
+++ src/commands/discord.ts
@@ -17,9 +17,7 @@ const discordLinkMap = new Map<string, {
 const convertDiscordMessage = (text: string) => {
   const rtlTextRegex = /([\u04c7-\u0591\u05D0-\u05EA\u05F0-\u05F4\u0600-\u06FF]+)/g;
   if (rtlTextRegex.test(text)) {
-    console.log('Converting RTL Text:', Buffer.from(text, 'utf-8').toString('hex'));
-    text = '\u202A' + text.replace(rtlTextRegex, '\u202C\u202B$1\u202C\u202A') + '\u202C';
-    console.log('Converted to:', Buffer.from(text, 'utf-8').toString('hex'));
+    text = text.replace(rtlTextRegex, '(RTL: $1)')
   }
   return text.replace(/\\/g, '').replace(/:(\w+):/g, (match, emojiName) => {
     for (const category of Object.keys(dismoji)) {
