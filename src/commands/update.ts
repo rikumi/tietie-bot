5630b7d47378fd198818935672abb10fd1cc706f
rikumiyu <rikumiyu@tencent.com>
2024-04-15 18:16:49 +0800

    feat: update
    security

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -12,10 +12,15 @@ const exec = async (command: string, options: cp.ExecOptions) => {
 };
 
 export const handleSlashCommand = async (ctx: ICommonMessageContext) => {
+  const branch = ctx.message.text?.split(/\s+/)[1];
   const cwd = path.resolve(__dirname, '..');
   const message = await (ctx as IContext).reply('代码更新执行中');
   const pullResult = await exec('git pull', { cwd });
   await ctx.telegram.editMessageText(message.chat.id, message.message_id, undefined, pullResult);
+  if (branch && /^[\w/]+$/.test(branch)) {
+    const switchResult = await exec(`git switch ${branch}`, { cwd });
+    await ctx.telegram.editMessageText(message.chat.id, message.message_id, undefined, switchResult);
+  }
   cp.spawnSync('pnpm', ['i'], { cwd });
   cp.spawn('npm', ['run', 'restart'], { cwd }).unref();
 };
