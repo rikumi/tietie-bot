ef618de94e46f234211d17a4fa3027cb3f6b1ae8
rikumiyu <rikumiyu@tencent.com>
2024-04-15 18:29:50 +0800

    back

--- src/commands/discord.ts
+++ src/commands/discord.ts
@@ -183,7 +183,7 @@ export const handleTelegramMessage = async (ctx: ICommonMessageContext, bot: IBo
 
   try {
     const formatUser = async (user: User) => {
-      const username = user.username?.toLowerCase() || `${user.first_name || ''} ${user.last_name || ''}`.trim();
+      const username = user.username /*?.toLowerCase()*/ || `${user.first_name || ''} ${user.last_name || ''}`.trim();
       return (await getDiscordNickname(chatId, userId)) || username;
     }
     const content = await tryDescribeMessage(message, bot, formatUser)
