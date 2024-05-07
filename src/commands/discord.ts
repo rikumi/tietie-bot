31f346c445bb6e8e93f278a233462eb7b3dd8c90
rikumiyu <rikumiyu@tencent.com>
2024-05-07 13:14:08 +0800

    feat: use single global discord bot to handle all messages

--- src/commands/discord.ts
+++ src/commands/discord.ts
@@ -10,8 +10,10 @@ import { User } from 'telegraf/typings/core/types/typegram';
 import { tryDescribeMessage } from 'src/modules/describe';
 import * as update from './update';
 
+let globalClient: discord.Client | undefined;
+let globalClientReady: Promise<void> | undefined;
+
 const discordLinkMap = new Map<string, {
-  client: discord.Client;
   discordChannelId: string;
   discordGuildId: string;
 }>();
@@ -42,41 +44,22 @@ if (!crypto.getRandomValues) {
   crypto.getRandomValues = getRandomValues as any; // usable
 }
 
-const createLinkBot = async (telegram: Telegram, chatId: string, discordChannelId: string, discordGuildId: string, echoResult = false) => {
-  if (discordLinkMap.has(chatId)) {
-    try {
-      discordLinkMap.get(chatId)!.client.close();
-    } catch (e) { }
-    discordLinkMap.delete(chatId);
+const initGlobalBot = async (telegram: Telegram, forceRestart = false) => {
+  if (globalClient) {
+    if (!forceRestart) return;
+    globalClient.close();
+    globalClient = undefined;
   }
-  // delay 3s before creating new client in case of rate limit
-  await new Promise(r => setTimeout(r, 3000));
-  const client = new discord.Client(config.discordUserToken);
-  discordLinkMap.set(chatId, {
-    client,
-    discordChannelId,
-    discordGuildId,
-  });
-
-  client.on.ready = () => {
-    if (!echoResult) return;
-    echoResult = false;
-
-    const guildInfo = client.info.guilds.find((guild: any) => guild.id === discordGuildId);
-    const channelInfo = guildInfo.channels.find((channel: any) => channel.id === discordChannelId);
-    const { name: guildName, member_count: memberCount } = guildInfo;
-    const { name: channelName, topic: channelTopic } = channelInfo;
-
-    telegram.sendMessage(chatId, `已链接到 Discord 频道：${guildName} #${channelName}，共 ${memberCount} 名成员。${channelTopic ? `\n主题：${channelTopic}` : ''}`);
-  };
-
-  client.on.message_create = (message: any) => {
+  globalClient = new discord.Client(config.discordUserToken);
+  globalClientReady = new Promise(r => globalClient.on.ready = r);
+  globalClient.on.message_create = (message: any) => {
     const channelId = String(message.channel_id);
-    if (channelId !== discordChannelId) return;
+    const chatId = Array.from(discordLinkMap.keys()).find(k => discordLinkMap.get(k)?.discordChannelId === channelId);
+    if (!chatId) return;
     if (message.author.username === config.discordUsername) return;
     const messageContent = convertDiscordMessage(message.content);
     const isServiceMessage = !message.author || message.author.bot;
-    const finalContent = isServiceMessage ? messageContent : `${ message.author.username }: ${ messageContent }`;
+    const finalContent = isServiceMessage ? messageContent : `${message.author.username}: ${ messageContent }`;
     if (!finalContent.includes(': ')) {
       telegram.sendMessage(chatId, finalContent);
       return;
@@ -86,22 +69,36 @@ const createLinkBot = async (telegram: Telegram, chatId: string, discordChannelI
       entities: [{ type: 'bold', offset: 0, length: nameUtf16Length }],
     });
   };
-
-  client.on.heartbeat_received = () => {
-    if (client._heartbeatStopTimeout) clearTimeout(client._heartbeatStopTimeout);
-    client._heartbeatStopTimeout = setTimeout(() => {
+  globalClient.on.heartbeat_received = () => {
+    if (globalClient._heartbeatStopTimeout) clearTimeout(globalClient._heartbeatStopTimeout);
+    globalClient._heartbeatStopTimeout = setTimeout(() => {
       console.log('_heartbeatStopTimeout');
-      createLinkBot(telegram, chatId, discordChannelId, discordGuildId);
+      initGlobalBot(telegram, true);
     }, 60000);
   };
-
-  client.on.message_edit = (message: any) => {
+  globalClient.on.message_edit = (message: any) => {
     const channelId = String(message.channel_id);
-    if (channelId !== discordChannelId) return;
+    const chatId = Array.from(discordLinkMap.keys()).find(k => discordLinkMap.get(k)?.discordChannelId === channelId);
+    if (!chatId) return;
     if (!message.interaction || message.interaction.name !== 'list') return;
     const messageContent = convertDiscordMessage(message.content);
     telegram.sendMessage(chatId, messageContent);
   };
+  await globalClientReady;
+};
+
+const createLinkBot = async (telegram: Telegram, chatId: string, discordChannelId: string, discordGuildId: string, forceRestart = false) => {
+  discordLinkMap.set(chatId, {
+    discordChannelId,
+    discordGuildId,
+  });
+  await initGlobalBot(telegram, forceRestart);
+  // send results
+  const guildInfo = globalClient.info.guilds.find((guild: any) => guild.id === discordGuildId);
+  const channelInfo = guildInfo.channels.find((channel: any) => channel.id === discordChannelId);
+  const { name: guildName, member_count: memberCount } = guildInfo;
+  const { name: channelName, topic: channelTopic } = channelInfo;
+  telegram.sendMessage(chatId, `已链接到 Discord 频道：${guildName} #${channelName}，共 ${memberCount} 名成员。${channelTopic ? `\n主题：${channelTopic}` : ''}`);
 };
 
 export const handleSlashCommand = async (ctx: ICommonMessageContext) => {
@@ -112,17 +109,15 @@ export const handleSlashCommand = async (ctx: ICommonMessageContext) => {
     if (!link) {
       return '本群未链接到任何 Discord 频道';
     }
-    link.client.close();
     createLinkBot(ctx.telegram, chatId, link.discordChannelId, link.discordGuildId, true);
     return;
   }
-
   if (!channelId || !/^\d+$/.test(channelId)) {
     (ctx as IContext).reply('用法：/discord <服务器 ID> <频道 ID>');
     return;
   }
   await setDiscordLink(chatId, channelId, guildId);
-  createLinkBot(ctx.telegram, chatId, channelId, guildId, true);
+  createLinkBot(ctx.telegram, chatId, channelId, guildId);
   const result = (await getDiscordLinks()).find(k => k.chatId === chatId);
   return `已尝试链接到 Discord 服务器 ${result?.discordGuildId} - 频道 ${result?.discordChannelId}`;
 };
@@ -130,7 +125,7 @@ export const handleSlashCommand = async (ctx: ICommonMessageContext) => {
 export const init = async (bot: IBot) => {
   const discordLinks = await getDiscordLinks();
   for (const link of discordLinks) {
-    createLinkBot(bot.telegram, link.chatId, link.discordChannelId, link.discordGuildId);
+    await createLinkBot(bot.telegram, link.chatId, link.discordChannelId, link.discordGuildId);
   }
 };
 
@@ -140,14 +135,14 @@ export const handleTelegramMessage = async (ctx: ICommonMessageContext, bot: IBo
   const userId = String(message.from.id);
   const link = discordLinkMap.get(chatId);
   if (!link) return false;
-  const { client, discordChannelId, discordGuildId } = link;
+  const { discordChannelId, discordGuildId } = link;
   if (/^\/update(\s|$)/.test(message.text!)) {
     return update.handleSlashCommand(ctx);
   }
   if (/^\/list(\s|$)/.test(message.text!)) {
-    const commands = await client.requester.fetch_request(
+    const commands = await globalClient.requester.fetch_request(
       `guilds/${discordGuildId}/application-command-index`,
-      undefined, client.clientData, 'GET'
+      undefined, globalClient.clientData, 'GET'
     );
     const listCommand = commands.application_commands.find((c: any) => c.name === 'list');
     const payload = {
@@ -165,7 +160,7 @@ export const handleTelegramMessage = async (ctx: ICommonMessageContext, bot: IBo
       nonce: String(Math.floor(Date.now() * 666666)),
     };
     try {
-      await client.call_check([]);
+      await globalClient.call_check([]);
     } catch (e: any) {
       (ctx as IContext).reply('转发消息失败：' + e.message, {
         reply_to_message_id: message.message_id,
@@ -173,7 +168,7 @@ export const handleTelegramMessage = async (ctx: ICommonMessageContext, bot: IBo
       });
       return;
     }
-    const interactionRes = await client.requester.fetch_request('interactions', payload);
+    const interactionRes = await globalClient.requester.fetch_request('interactions', payload);
     console.log('Sent Discord interaction:', interactionRes);
     const { message_id: messageId } = await (ctx as IContext).reply('已发送 /list 指令给 Discord 侧，请等待回应。本消息将自动删除。');
     await new Promise(r => setTimeout(r, 3000));
@@ -187,7 +182,7 @@ export const handleTelegramMessage = async (ctx: ICommonMessageContext, bot: IBo
       return (await getDiscordNickname(chatId, userId)) || username;
     }
     const content = await tryDescribeMessage(message, bot, formatUser)
-    client.send(discordChannelId, { content });
+    globalClient.send(discordChannelId, { content });
   } catch (e) {
     console.error('转发指令失败', e);
   }
