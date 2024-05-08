a9a8836503fc25cd687af4549d01b2b7b7cdb42b
rikumiyu <rikumiyu@tencent.com>
2024-05-08 13:56:08 +0800

    fix

--- src/commands/short.ts
+++ src/commands/short.ts
@@ -5,5 +5,8 @@ export const USAGE = `<url> 缩短一个 url，请勿滥用`;
 
 export const handleSlashCommand = async (message: GenericMessage) => {
   const url = message.text.split(/\s+/)[1];
+  if (!url || !/^https?:\/\/.+/.test(url)) {
+    return '请输入要缩短的原始 URL。';
+  }
   return await createShortUrl(url);
 };
