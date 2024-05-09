7b47ec8e5557ffe1a5fb7cc6fbeb1a43a82a7d75
Rikumi <riku.mi@outlook.com>
2024-05-09 19:55:03 +0800

    Update telegram-file.ts

--- src/server/routes/telegram-file.ts
+++ src/server/routes/telegram-file.ts
@@ -2,6 +2,7 @@ import { IncomingMessage, ServerResponse } from 'http';
 import telegramClient from '../../clients/telegram';
 import { request } from 'https';
 import { getTelegramFileId } from 'src/database/tgfile';
+import lottie from 'lottie-converter';
 
 export const ROUTE = /^\/(tgfile|tguniq)\/(.+)\/(.*?)$/;
 
@@ -21,6 +22,22 @@ const fileHandler = async (req: IncomingMessage, res: ServerResponse) => {
   console.log('[Server] TelegramFileHandler fetching url:', url.toString());
   const fetchRes = await new Promise<IncomingMessage>(r => request(url).on('response', r).end());
 
+  if (mimeType === 'application/tgs+gzip') {
+    const converted = Buffer.from(await lottie({
+      file: Buffer.from(await fetchRes.arrayBuffer()),
+      format: 'mp4',
+      width: 512,
+      height: 512,
+    }), 'base64');
+    res.writeHead(200, 'OK', {
+      'content-type': 'video/webm',
+      'content-length': converted.length,
+    });
+    res.write(converted);
+    res.end();
+    return;
+  }
+  
   console.log('[Server] TelegramFileHandler got headers:', mimeType, fetchRes.headers['content-length']);
   res.writeHead(200, 'OK', {
     'content-type': mimeType,
