6bfc059672b7f0ea3af50fed594a3472b7442908
rikumiyu <rikumiyu@tencent.com>
2024-06-30 18:03:41 +0800

    fix

--- src/commands/count.ts
+++ src/commands/count.ts
@@ -8,8 +8,8 @@ export const handleSlashCommand = async (message: GenericMessage) => {
   const stackCount = parseInt(RegExp.$1) || 64;
   if (!content) return;
   let remaining = evaluate(content);
-  return [stackCount, 27, 27, Infinity]
+  return [stackCount, 27, 27, 2, Infinity]
     .map((k, i) => ([remaining, i] = [Math.floor(remaining / k), remaining % k])[1])
-    .map((value, level) => value ? value + '个组盒桶'[level] : '')
+    .map((value, level) => value ? value + ['组', '盒', '桶装盒', '大箱盒'][level] : '')
     .reverse().filter(Boolean).join('零');
 };
