7186066e99d5793f2c8ec73bfc6d0b2205a18618
rikumiyu <rikumiyu@tencent.com>
2024-07-13 09:18:41 +0800

    feat: test push

--- src/server/routes/github-webhook.ts
+++ src/server/routes/github-webhook.ts
@@ -6,6 +6,7 @@ export const ROUTE = '/github-webhook';
 const githubWebhookHandler = async (req: IncomingMessage, res: ServerResponse) => {
   const body = JSON.parse((await getStreamContent(req)).toString('utf-8'));
   console.log('/github-webhook', JSON.stringify(body, null, 2));
+  // TODO
   res.writeHead(200);
   res.end();
 };
