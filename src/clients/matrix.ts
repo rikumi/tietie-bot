0fe8b4867add684fd22b722519c039154d8225ab
rikumiyu <rikumiyu@tencent.com>
2024-09-23 00:34:50 +0800

    fix: ignore events other than room.message and sticker from matrix

--- src/clients/matrix.ts
+++ src/clients/matrix.ts
@@ -27,11 +27,8 @@ export class MatrixUserBotClient extends EventEmitter implements GenericClient<a
     const homeServer = config.matrixHomeServer || 'matrix.org';
     const accessToken = config.matrixAccessToken;
     this.bot = new MatrixClient('https://' + homeServer, accessToken, storage);
-    this.bot.on('room.event', async (roomId: string, message: any) => {
-      const transformedMessage = await this.transformMessage(message, roomId);
-      if (transformedMessage.userId === this.botInfo?.user_id) return;
-      this.emit(message.content['m.new_content'] ? 'edit-message' : 'message', transformedMessage);
-    });
+    this.bot.on('room.message', this.handleMessage);
+    this.bot.on('sticker', this.handleMessage);
     AutojoinRoomsMixin.setupOnClient(this.bot);
     this.fetchBotInfo();
 
@@ -48,6 +45,12 @@ export class MatrixUserBotClient extends EventEmitter implements GenericClient<a
   public async stop(): Promise<void> {
     this.bot.stop();
   }
+
+  public handleMessage = async (roomId: string, message: any) => {
+    const transformedMessage = await this.transformMessage(message, roomId);
+    if (transformedMessage.userId === this.botInfo?.user_id) return;
+    this.emit(message.content['m.new_content'] ? 'edit-message' : 'message', transformedMessage);
+  };
 
   public async sendMessage(message: MessageToSend): Promise<GenericMessage> {
     if (message.rawUserDisplayName) {
