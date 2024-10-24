f62b17429ef842d3c89af09c6e773ce1f00b8e2a
Rikumi <riku.mi@outlook.com>
2024-10-24 20:28:10 +0800

    add command

---
+++ src/commands/nbnhhsh.ts
@@ -0,0 +1,10 @@
+import { GeneralMessage } from '../clients/base';
+
+export const handleSlashCommand = async (message: GeneralMessage) => {
+  const text = message.text.replace(/^\S+/, '');
+  const res = await fetch('https://lab.magiconch.com/api/nbnhhsh/guess', {
+    method: 'POST',
+    body: JSON.stringify({ text }),
+  });
+  return (await res.json()).map(({ name, trans }) => `${name}: ${trans.join(' ')}`).join('\n') || 'bn';
+};
