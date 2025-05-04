74be7de47e1210ac3dc9ae34f8fe6505d90b1470
Rikumi <riku.mi@outlook.com>
2025-05-04 18:46:19 +0800

    Update pk.ts

--- src/commands/pk.ts
+++ src/commands/pk.ts
@@ -3,7 +3,7 @@ import crypto from 'crypto';
 
 export const USAGE = `从给出的多个选项中找出 SHA256 最大的一个`;
 
-const hash = (text: string) => crypto.createHash('sha256').update(string).digest().toString('hex');
+const hash = (text: string) => crypto.createHash('sha256').update(text).digest().toString('hex');
 
 export const handleSlashCommand = (message: GenericMessage) => {
   const content = message.text.split(/\s+/).slice(1);
